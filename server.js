9e991b4526ca6d1aa66e1fd3d93c26a0c7779133
Hosea Simanjuntak <hosea.simanjuntak@anabatic.com>
2018-11-14 20:31:55 +0700

    add get-total-users and get-user-statistic

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const loginSuccess = JSON.parse(fs.readFileSync('./sample/login/success.json', '
 const loginError = JSON.parse(fs.readFileSync('./sample/login/error.json', 'UTF-8'))
 const successForgotDb = JSON.parse(fs.readFileSync('./sample/forgotpassword/success.json', 'UTF-8'))
 const errorForgotDb = JSON.parse(fs.readFileSync('./sample/forgotpassword/error.json', 'UTF-8'))
+const getTotalUsersSuccess = JSON.parse(fs.readFileSync('./sample/get-total-users/success.json', 'UTF-8'))
+const getUserStatisticSuccess = JSON.parse(fs.readFileSync('./sample/get-user-statistic/success.json', 'UTF-8'))
+const getUserStatisticError = JSON.parse(fs.readFileSync('./sample/get-user-statistic/error.json', 'UTF-8'))
 
 //server.use(bodyParser.urlencoded({extended: true}))
 //server.use(bodyParser.json())
@@ -25,12 +28,21 @@ server.post('/api/user/login', (req, res) => {
     res.status(200).json(loginError)
   }
 })
-
 server.post('/api/user/get-by-username', (req, res) => {
   if(req.body.username === USERNAME)
     res.status(200).json(successForgotDb)
   else {
     res.status(200).json(errorForgotDb)
+  }
+})
+server.post('/api/user/get-total-users', (req, res) => {
+  res.status(200).json(getTotalUsersSuccess)
+})
+server.post('/api/user/get-user-statistic', (req, res) => {
+  if(req.body.type === "ANNUAL")
+    res.status(200).json(getUserStatisticSuccess)
+  else {
+    res.status(200).json(getUserStatisticError)
   }
 })
 
